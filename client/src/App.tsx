fc58ca610dbc0509f41b3a08a8df8d7fb60f9091
unknown <emilygb88@gmail.com>
2023-11-21 18:10:34 -0500

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1> Welcome to Hackademia </h1>
+      <form>
+      <label>Enter your name:
+        <input type="text" />
+      </label>
+    </form>
+    </>
+  );
+}
+
+export default App;
